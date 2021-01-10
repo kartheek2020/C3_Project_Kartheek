a5bb54b0226a3888f05c681a634dfd331111d8cb
kartheek2020 <74132972+kartheek2020@users.noreply.github.com>
2021-01-10 12:21:23 -0500

    Update RestaurantTest.java
    Part 3: Solution

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -57,4 +57,21 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void searching_for_existing_item_should_return_order_value(){
+        Item i1 = restaurant.findItemByName("Sweet corn soup");
+        Item i2 = restaurant.findItemByName("Vegetable lasagne");
+
+        int orderValue = i1.getPrice() + i2.getPrice();
+        assertEquals(388,orderValue);
+    }
+
+    @Test
+    public void searching_for_existing_item_should_return_wrong_order_value(){
+        Item i1 = restaurant.findItemByName("Sweet corn soup");
+        Item i2 = restaurant.findItemByName("Vegetable lasagne");
+
+        int orderValue = i1.getPrice() + i2.getPrice();
+        assertEquals(398,orderValue);
+    }
 }
